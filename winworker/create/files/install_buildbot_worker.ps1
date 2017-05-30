0464fb7a28e70918adce28b09028c36a72af613f
michael starke <michael.starke@hicknhack-software.com>
2017-05-30 16:38:07 +0200

    Using native .Net method

--- winworker/create/files/install_buildbot_worker.ps1
+++ winworker/create/files/install_buildbot_worker.ps1
@@ -1,10 +1,10 @@
-pip install buildbot-worker==0.9.0.post1 --upgrade --no-binary buildbot-worker
+pip install buildbot-worker==0.9.7 --upgrade --no-binary buildbot-worker
 $x = $LASTEXITCODE
 if (0 -eq $x) {
   # reload paths
   $paths = 'Machine', 'User' |
     % {
-      (Get-EnvironmentVariable -Name 'PATH' -Scope $_) -split ';'
+      ( [environment]::GetEnvironmentVariable('PATH',$_) ) -split ';'
     } |
     Select -Unique
   $Env:PATH = $paths -join ';'
