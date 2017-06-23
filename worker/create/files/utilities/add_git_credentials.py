cbbf2c245d69e2fa01ed20c68b82fb4fc7db5cb5
michael starke <michael.starke@hicknhack-software.com>
2017-06-23 14:01:32 +0200

    using version 0.9.8

--- worker/create/files/utilities/add_git_credentials.py
+++ worker/create/files/utilities/add_git_credentials.py
@@ -6,7 +6,7 @@ import os.path
 if not len(sys.argv) == 2:
 	sys.exit(1)
 
-credentials_file = os.path.expanduser('~') + '.git-credentials'
+credentials_file = os.path.normpath(os.path.expanduser('~') + '/.git-credentials')
 credentials = sys.argv[1]
 
 with open(credentials_file, 'ab') as f:
